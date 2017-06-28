34001e852af3e4422396e77099c64f651d411f48
farhanahameed <farhanahameed30@gmail.com>
2017-06-28 21:18:40 +0530

    Create Evo.java

---
+++ Evo.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Evo
+{
+	public static void main(String args[])
+	{
+		int num;
+		num=Integer.parseInt(args[0]);
+			if(num%2==0)
+			{
+				System.out.println("The given number is even");
+			}
+			else
+			{
+				System.out.println("The given number is odd");
+			}
+	}
+}
