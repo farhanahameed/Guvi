daf3d5274a1d0920b69b3721da4d550ddd9fa768
farhanahameed <farhanahameed30@gmail.com>
2017-06-30 20:02:17 +0530

    Create Hai.java

---
+++ Hai.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Hai
+{
+	public static void main (String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		for(int i=0;i<5;i++)
+		{
+		    System.out.print("Hello");
+	    }
+	}
+}
