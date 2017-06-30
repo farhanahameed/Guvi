b2f6e88ec277dfa4e8f0a235af82cd3b0a32538e
farhanahameed <farhanahameed30@gmail.com>
2017-06-30 20:42:19 +0530

    Create Num.java

---
+++ Num.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Num {
+	public static void main (String[] args)
+	{
+		int n=Integer.parseInt(args[0])
+		int sum=0;
+		n=n-1;
+		sum=n*(n+1)/2;
+		System.out.print(sum);
+	}
+}
