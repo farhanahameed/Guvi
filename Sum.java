bbceb054e64fd9b5ec75ec5ec45b82ea83563af8
farhanahameed <farhanahameed30@gmail.com>
2017-07-02 18:46:55 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,15 @@
+    import java.io.*;
+    import java.util.*;
+    class Sum
+    {
+    	p;ublic static void main(String args[])
+    	{
+    		int n,i,sum=o;
+    		n=Integer.parseInt(args[]);
+    		for(i=o;i<n;i++)
+    		{
+    			sum+=i;
+    		}
+    		System.out.println("sum"+sum);
+    	}
+    }
