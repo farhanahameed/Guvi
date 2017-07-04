91db87dc6de785acdabd9838c14cc00b84fa08c0
farhanahameed <farhanahameed30@gmail.com>
2017-07-04 21:21:48 +0530

    Create FindPower.java

---
+++ FindPower.java
@@ -0,0 +1,9 @@
+    import java.util.*;
+    import java.io.*;
+    public class FindPower 
+    {
+      public static void main(String[] args)
+      {
+         System.out.println(Math.pow(2,2));
+      }
+    }
