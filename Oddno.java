599f062a6542a935ff7b2a201f9f6bdbedd7bf07
farhanahameed <farhanahameed30@gmail.com>
2017-07-06 21:48:05 +0530

    Create Oddno.java

---
+++ Oddno.java
@@ -0,0 +1,18 @@
+    import java.io.*;
+    import java.util.*;
+    class Oddno
+    {
+    	public static void main(String args[])
+    	{
+    		int a,b;
+    		a=Integer.parseInt(args[0]);
+    		b=Integer.parseInt(args[1]);
+    		for(int i=a;i<=b;i++)
+    		{
+    			if(i%2!=0)
+    			{
+    			System.out.println(i);
+    			}
+    		}
+    	}
+    }
