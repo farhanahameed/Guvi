ea8030b264c8ef2598b76e4a5c878d67ae65bc7e
farhanahameed <farhanahameed30@gmail.com>
2017-07-06 21:12:38 +0530

    Create Primeno.java

---
+++ Primeno.java
@@ -0,0 +1,17 @@
+    import java.io.*;
+    import java.util.*;
+    class Primeno
+    {
+    	public static void main(String args[])
+    	{
+    		int n=Integer.parseInt(args[0])
+    		if(n%1==0)
+    		{
+    			System.out.println("The number is Prime");
+    		}
+    		else
+    		{
+    			System.out.println("The number is not a Prime");
+    		}
+    	}
+    }
