44c1b9ae32022ac9b6f571392edc0ba562929d9e
farhanahameed <farhanahameed30@gmail.com>
2017-12-02 18:00:32 +0530

    Create fac.java

---
+++ fac.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class fac
+{
+	public static void main (String[] args) 
+	{
+	int i,fact=1;  
+            int num=6;//It is the number to calculate factorial    
+            for(i=1;i<=num;i++){    
+            fact=fact*i;    
+            }    
+	System.out.println(fact);
+	}
+}
