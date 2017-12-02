df0892223ceb1ac2c8cef326a4099acbd282a2cc
farhanahameed <farhanahameed30@gmail.com>
2017-12-02 17:51:39 +0530

    Create rev.java

---
+++ rev.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) 
+	{
+	StringBuffer str=new StringBuffer("Farhana");
+	System.out.println(str.reverse());
+	}
