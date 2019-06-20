e80b9c5749bbf5916ade68c73dbda686bc624570
farhanahameed <farhanahameed30@gmail.com>
2019-06-20 10:48:40 +0530

    Create SmallArry.java

---
+++ SmallArry.java
@@ -0,0 +1,31 @@
+import java.io.*;
+//import java.lang.*;
+import java.util.*;
+public class SmallArry
+{
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int temp=0,b=0;
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=in.nextInt();
+		}
+		for(int j=0;j<n;j++)
+		{
+			for(int k=0;k<n;k++)
+			{
+				if(a[j]>a[k])
+				{
+					temp=a[k];
+					a[k]=a[j];
+					a[j]=temp;
+				}
+			}
+			b=a[j];
+		}
+		System.out.println(b);
+	}
+}
